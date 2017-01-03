25c797158d6d0c6032a174f3da0085d06d15a147
BrianHGrant <bhgrant8@gmail.com>
2017-01-02 23:09:04 -0800

    add RESTful routing

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -17,6 +17,27 @@ public class App {
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
 
+    get("tasks/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/task-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/tasks", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("tasks", Task.all());
+      model.put("template", "templates/tasks.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/tasks/:id", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Task task = Task.find(Integer.parseInt(request.params(":id")));
+      model.put("task", task);
+      model.put("template", "templates/task.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
     post("/tasks", (request, response) -> {
       Map<String, Object> model = new HashMap<String, Object>();
 
