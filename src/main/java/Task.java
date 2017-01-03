5d91d21e5e6ffd3e171847f9cb8e20c96d88556a
BrianHGrant <bhgrant8@gmail.com>
2017-01-02 22:27:53 -0800

    add timestamp and completed status

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,12 +1,26 @@
+import java.time.LocalDateTime;
+
 public class Task {
   private String mDescription;
+  private boolean mCompleted;
+  private LocalDateTime mCreatedAt;
 
   public Task(String description) {
     mDescription = description;
+    mCompleted = false;
+    mCreatedAt = LocalDateTime.now();
   }
 
   public String getDescription() {
     return mDescription;
+  }
+
+  public boolean isCompleted() {
+    return mCompleted;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return mCreatedAt;
   }
 
 }
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,5 +1,6 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.time.LocalDateTime;
 
 public class TaskTest {
 
@@ -13,5 +14,15 @@ public class TaskTest {
   public void Task_instantiatesWithDescription_String() {
     Task myTask = new Task("Mow the lawn");
     assertEquals("Mow the lawn", myTask.getDescription());
+  }
+
+  @Test public void isCompleted_isFalseAfterInstantiation_false() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(false, myTask.isCompleted());
+  }
+
+  @Test public void getCreatedAt_instantiatiesWithCurrentTime_today() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(LocalDateTime.now().getDayOfWeek(), myTask.getCreatedAt().getDayOfWeek());
   }
 }
